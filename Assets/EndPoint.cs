2d7f82c036e487f58dc781d0e224e8be97bdef1b
Cole Druffel <Druffel12@gmail.com>
2018-04-03 18:02:09 -0700

    Initial Commit

---
+++ Assets/EndPoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class EndPoint : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    public GameObject end;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = end.transform.position;
+    }
+
+
+    void Update()
+    {
+
+    }
+}
